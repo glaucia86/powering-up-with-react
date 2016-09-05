a719538f758ed6fca9c9d75aa6c2eb9d3b096e03
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2016-09-04 21:55:57 -0300

    Criando listas de maneira dinâmica usando o React.Js & JSX

--- Level 1 - First Component/1.5 -/components.js
+++ Level 1 - First Component/1.5 -/components.js
@@ -1,10 +1,20 @@
 /* Criação do Primeiro Componente */
 class RobotBox extends React.Component {
     render() {
+        const pi = Math.PI;
+        const topics = ["React", "JSX", "JavaScript", "Programação"];
         return (
             <div>
                 <h3>Me chamo McCircuit</h3>
                 <p>Estou aqui para te ajudar!!</p>
+                    <div className='is-tasty-pie'>
+                        O valor aproximado de PI é: {pi}
+                    </div>
+                    <h3>Tópicos que estou interessado</h3>
+                    /* Aqui estou listando de maneira dinâmica, ao usar o método map() */
+                    <ul>
+                        { topics.map( top => <li>{top}</li>) }
+                    </ul>
             </div>
         );
     }
--- README.md
+++ README.md
@@ -4,7 +4,7 @@ Curso de React.Js do Code School com o objetivo de aprender as features com resp
 
 Exercícios realizados durante o curso com os códigos de cada nível. :)
 
-##### 1) First Component - [Level 1]()
+##### 1) First Component - [Level 1](https://github.com/glaucia86/powering-up-with-react/tree/master/Level%201%20-%20First%20Component/1.5%20-)
 ##### 2) Talk Through Props
 ##### 3) Component State
 ##### 4) Synthetic Events
