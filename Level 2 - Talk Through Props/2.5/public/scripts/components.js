15754df7e4fcb3442408621b8bd6b021c95f821b
oi393785 <glaucia.lemos@oi.net.br>
2016-09-06 18:13:48 -0300

    Criação de um método que permita saber qual é o post mais popular

--- Level 2 - Talk Through Props/2.5/public/scripts/components.js
+++ Level 2 - Talk Through Props/2.5/public/scripts/components.js
@@ -6,6 +6,7 @@ class CommentBox extends React.Component {
         return(
             <div className="comment-box">
                 <h3>Comentários</h3>
+                    { this._getPopularMessage(comments.length) } 
                 <h4 className="comment-count">{comments.length} comentários</h4>
                 <div className="comment-list">
                     {comments}
@@ -14,7 +15,18 @@ class CommentBox extends React.Component {
         );
     }
 
-    /* Método para para retornar o array dos elementos do JSX */
+    /* Método para poder retornar o post mais popular enviado  */
+    _getPopularMessage(commentCount) {
+        const POPULAR_COUNT = 10;
+
+        if(commentCount > POPULAR_COUNT) {
+            return (
+              <div>Este post é bastante popular! Fique por dentro desse post!!!</div>  
+            );
+        }
+    }
+
+    /* Método para poder retornar o array dos elementos do JSX */
     _getComments() {
         const commentList = [
             { id:1, author: 'Jake Luc', body: 'Simplesmente não curto!', avatarUrl:'images/default-avatar.png' },
@@ -35,6 +47,7 @@ class Comment extends React.Component {
     render() {
         return (
             <div className="comment">
+                <img src={ this.props.avatarUrl } alt={`${this.props.author} fotos`} />
                 <p className="comment-header">{this.props.author}</p>
                 <p className="comment-body">{this.props.body}</p>
                 <div className="comment=actions">
