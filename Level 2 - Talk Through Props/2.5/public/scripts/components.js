35d40b884636d91e0bc79a9de8da2eb8fe741163
oi393785 <glaucia.lemos@oi.net.br>
2016-09-06 14:34:24 -0300

    Redefinição do escopo do esqueleto do projeto e alteração no arqivo 'package.json'

--- Level 2 - Talk Through Props/2.5/public/scripts/components.js
+++ Level 2 - Talk Through Props/2.5/public/scripts/components.js
@@ -1,4 +1,19 @@
-/* Criação do Componente */
+/* Componente - Caixa de Comentários */
+class CommentBox extends React.Component {
+    render() {
+        return (
+            <div className="comment-box">
+                <h3>Comentários</h3>
+                <h4 className="comment-count">2 Comentários</h4>
+                <div className="comment-list">
+                    <Comment author='Anne Droid' body='Eu quero saber o que é amor...' />
+                </div>
+            </div>
+        );
+    }
+}
+
+/* Componente - Comentários */
 class Comments extends React.Component {
     render() {
         return (
