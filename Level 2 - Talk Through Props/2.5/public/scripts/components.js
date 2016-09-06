41a1ffb3639ae817175ca20d273583b893f31383
oi393785 <glaucia.lemos@oi.net.br>
2016-09-06 17:46:46 -0300

    Inclusão do método '_getComments()' para poder mostrar os elementos do JSX

--- Level 2 - Talk Through Props/2.5/public/scripts/components.js
+++ Level 2 - Talk Through Props/2.5/public/scripts/components.js
@@ -1,30 +1,43 @@
 /* Componente - Caixa de Comentários */
 class CommentBox extends React.Component {
     render() {
-        return (
+        /* Aqui é para poder mostrar os comentários disponíveis */
+        const comments = this._getComments() || [];
+        return(
             <div className="comment-box">
                 <h3>Comentários</h3>
-                <h4 className="comment-count">2 Comentários</h4>
+                <h4 className="comment-count">{comments.length} comentários</h4>
                 <div className="comment-list">
-                    <Comment author='Anne Droid' body='Eu quero saber o que é amor...' />
+                    {comments}
                 </div>
             </div>
         );
     }
+
+    /* Método para para retornar o array dos elementos do JSX */
+    _getComments() {
+        const commentList = [
+            { id:1, author: 'Jake Luc', body: 'Simplesmente não curto!', avatarUrl:'images/default-avatar.png' },
+            { id:2, author: 'Rod Droid', body:'Eu gostaria de saber o que é amor....', avatarUrl:'images/default-avatar.png' }
+        ];
+
+        return commentList.map((comment) => {
+            return (<Comment
+                    author      = { comment.author }
+                    body        = { comment.body }
+                    avatarUrl   = { comment.avatarUrl } 
+                    key         = { comment.id } />);
+        });
+    }
 }
 
-/* Componente - Comentários */
-class Comments extends React.Component {
+class Comment extends React.Component {
     render() {
         return (
             <div className="comment">
-                <p className="comment-header">
-                    {this.props.author}
-                </p>
-                <p className="comment-body">
-                    {this.props.body}
-                </p>
-                <div className="comment-actions">
+                <p className="comment-header">{this.props.author}</p>
+                <p className="comment-body">{this.props.body}</p>
+                <div className="comment=actions">
                     <a href="#">Excluir Comentário</a>
                 </div>
             </div>
@@ -34,4 +47,4 @@ class Comments extends React.Component {
 
 let target = document.getElementById('comment-app');
 
-ReactDOM.render( <Comments/>, target);
+ReactDOM.render( <CommentBox/>, target);
